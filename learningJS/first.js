5682226a15df71a5989b9923c8266d9e5858ad18
saksham <saksham82878287@gmail.com>
2024-02-04 16:51:01 +0530

    started js

--- learningJS/first.js
+++ learningJS/first.js
@@ -1,6 +1,17 @@
-console.log('hey')
-setTimeout(function() {
-    console.log('hey2')
-}, 0)
-console.log('hey3')
-console.log('hey4')
+// console.log('hey')
+// setTimeout(function() {
+//     console.log('hey2')
+// }, 0)
+// console.log('hey3')
+// console.log('hey4')
+
+const a = 9
+const b = 4
+sum(a, b)
+
+function sum(a, b){
+    const sum = a + b
+    console.log(sum)
+}
+
+
